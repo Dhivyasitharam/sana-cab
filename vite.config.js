3bbff24a3e72ce9a15c8d36ad80937791cb67f54
Dhivya Bharathi <your-email@example.com>
2026-06-18 12:17:26 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/sana-cab/',
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "http://localhost:4000",
     },
   },
 })
