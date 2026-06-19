6212d4d0b4391dc8318878b926ab1a0735cc3d6e
Dhivya Bharathi <your-email@example.com>
2026-06-19 11:35:07 +0530

    Switch to email notifications, fix nodemailer version

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/sana-cab/",
+  base: "/",
   server: {
     proxy: {
       "/api": "http://localhost:4000",
